637fed022a60bf0786ed9f43ea3bd7f4a702b82e
Egor Grinevich <yahor_hrynevich@velmie.com>
2025-09-01 14:17:22 +0300

    feat: sign it tests

--- cypress/e2e/sing-in.cy.js
+++ cypress/e2e/sing-in.cy.js
@@ -1,9 +1,30 @@
-describe('Authentication', () => {
-  it('Logs in', () => {
-    cy.visit('http://localhost:4200')
+describe('Sign in', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200');
+  });
+
+  it('Displays login form', () => {
+    cy.get('input[type="email"]').should('exist');
+    cy.get('input[type="password"]').should('exist');
+    cy.get('button[type="submit"]').should('exist');
+  });
+
+  it('Logs in successfully with valid credentials', () => {
     cy.get('input[type="email"]').type('admin@email.com');
     cy.get('input[type="password"]').type('P@ssword1!');
-    cy.get('button[type="submit"]').click()
-    cy.get('h2').should('have.text', 'Финансы')
-  })
-})
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should('include', '/admin/dashboard');
+    cy.get('h2').should('have.text', 'Финансы');
+  });
+
+  it('Logs out successfully', () => {
+    cy.get('input[type="email"]').type('admin@email.com');
+    cy.get('input[type="password"]').type('P@ssword1!');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('button.logout-button').click();
+    cy.url().should('include', '');
+    cy.get('input[type="email"]').should('exist');
+  });
+});
